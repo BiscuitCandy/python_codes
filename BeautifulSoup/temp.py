5562db65ec1e76cfda4a301a65e9b854b3371908
BiscuitCandy <70342294+BiscuitCandy@users.noreply.github.com>
2021-03-26 16:26:46 +0530

    Update temp.py

--- BeautifulSoup/temp.py
+++ BeautifulSoup/temp.py
@@ -12,7 +12,7 @@ soup = BeautifulSoup(r.content, 'html.parser')
 print(soup.title.text)
 
 details = soup.select('div.crayons-story__indention')
-with open(r'C:\Users\VIVEK VR\Documents\Python\BeautifulSoup\articles.txt', 'w') as f:
+with open(r'\articles.txt', 'w') as f:
     for block in details :
         header = block.select('h2.crayons-story__title > a')[0].text.strip()
         link = block.select('h2.crayons-story__title > a')[0].get('href')
